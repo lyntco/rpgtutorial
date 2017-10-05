b68292621ea21fa32dacb15ee9af9d01342ba754
Larlyn Tanasap <strawww@gmail.com>
2017-10-05 15:46:08 -0700

    Add enemies and sword

---
+++ Assets/Scripts/HurtEnemy.cs
@@ -0,0 +1,22 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class HurtEnemy : MonoBehaviour {
+
+	private GameObject enemy;
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D (Collider2D other) {
+		if (other.gameObject.tag == "Enemy") {
+			Destroy(other.gameObject); // KILL THE ENEMY
+			enemy = other.gameObject;
+		}
+	}
+}
---
+++ Assets/Scripts/SlimeController.cs
@@ -0,0 +1,66 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+
+public class SlimeController : MonoBehaviour {
+	public float moveSpeed;
+	public float timeBetweenMove;
+	public float timeToMove;
+	public float waitToReload;
+
+	private Rigidbody2D rigidbody;
+	private bool moving;
+	private Vector3 moveDirection;
+	private float timeBetweenMoveCounter;
+	private float timeToMoveCounter;
+	private bool reloading;
+	private GameObject player;
+
+	void Start () {
+		rigidbody = GetComponent<Rigidbody2D>();
+		// timeBetweenMoveCounter = timeBetweenMove;
+		// timeToMoveCounter = timeToMove;
+		timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.15f, timeBetweenMove * 1.55f);
+		timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
+		moving = Random.value < 0.5;
+	}
+
+	void Update () {
+		if (moving) {
+			timeToMoveCounter -= Time.deltaTime;
+			rigidbody.velocity = moveDirection;
+			if (timeToMoveCounter < 0) {
+				moving = false;
+				timeBetweenMoveCounter = Random.Range(timeBetweenMove * 0.75f, timeBetweenMove * 1.25f);
+			}
+		} else {
+			timeBetweenMoveCounter -= Time.deltaTime;
+			rigidbody.velocity = Vector2.zero;
+			if (timeBetweenMoveCounter < 0f) {
+				moving = true;
+				timeToMoveCounter = Random.Range(timeToMove * 0.75f, timeToMove * 1.25f);
+				float randomX = Random.Range(-1f, 1f) * moveSpeed;
+				float randomY = Random.Range(-1f, 1f) * moveSpeed;
+				moveDirection = new Vector3(randomX, randomY, 0f);
+			}
+		}
+		if (reloading) {
+			waitToReload -= Time.deltaTime;
+			if (waitToReload < 0) {
+				SceneManager.LoadScene(SceneManager.GetActiveScene().buildIndex);
+				player.SetActive(true);
+			}
+		}
+	}
+
+	void OnCollisionEnter2D (Collision2D other) {
+		if (other.gameObject.name == "Player") {
+			// Destroy(other.gameObject); // KILL THE PLAYER
+			other.gameObject.SetActive(false);
+			reloading = true;
+			player = other.gameObject;
+		}
+	}
+}
+
