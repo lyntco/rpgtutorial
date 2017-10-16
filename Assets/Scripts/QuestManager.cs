b324a803e3af8135c920d5ec41553a571f306dbf
Larlyn Tanasap <strawww@gmail.com>
2017-10-16 09:58:43 -0700

    Add quest system

---
+++ Assets/Scripts/QuestManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestManager : MonoBehaviour {
+
+	public QuestObject[] quests;
+	public bool[] questsCompleted;
+	public DialogueManager dialogueMManager;
+
+	void Start () {
+		questsCompleted = new bool[quests.Length];
+	}
+
+	void Update () {
+
+	}
+
+	public void ShowQuestText(string questText) {
+		dialogueMManager.dialogueLines = new string[1];
+		dialogueMManager.dialogueLines[0] = questText;
+		dialogueMManager.currentLine = 0;
+		dialogueMManager.ShowDialogue();
+	}
+}
---
+++ Assets/Scripts/QuestObject.cs
@@ -0,0 +1,29 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class QuestObject : MonoBehaviour {
+	public int questNumber;
+	public QuestManager questManager;
+	public DialogueManager dialogueManager;
+	public string startText;
+	public string endText;
+
+	void Start () {
+
+	}
+
+	void Update () {
+
+	}
+
+	public void StartQuest() {
+		questManager.ShowQuestText(startText);
+	}
+
+	public void EndQuest() {
+		questManager.ShowQuestText(endText);
+		questManager.questsCompleted[questNumber] = true;
+		gameObject.SetActive(false);
+	}
+}
