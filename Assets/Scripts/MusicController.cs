e2b06519c114b4307f9795392d80daa1bbebe0cf
Larlyn Tanasap <strawww@gmail.com>
2017-10-17 16:01:54 -0700

    Add background music

---
+++ Assets/Scripts/MusicController.cs
@@ -0,0 +1,36 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicController : MonoBehaviour {
+	private SFXManager sFXManager;
+	private static bool musicControllerExists;
+	public AudioSource[] musicTracks;
+	public int currentTrack;
+
+	public bool musicCanPlay;
+
+	void Start () {
+		if (!musicControllerExists) {
+			musicControllerExists = true;
+			DontDestroyOnLoad(transform.gameObject);
+		} else {
+			Destroy(gameObject);
+		}
+	}
+
+	void Update () {
+		if (musicCanPlay) {
+			if (!musicTracks[currentTrack].isPlaying) {
+				musicTracks[currentTrack].Play();
+			}
+		} else {
+			musicTracks[currentTrack].Stop();
+		}
+	}
+
+	public void SwitchTrack(int trackNumber) {
+		musicTracks[currentTrack].Stop();
+		currentTrack = trackNumber;
+	}
+}
---
+++ Assets/Scripts/MusicSwitcher.cs
@@ -0,0 +1,28 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class MusicSwitcher : MonoBehaviour {
+	private MusicController musicController;
+	public int newTrack;
+	public bool switchOnStart;
+
+	void Start () {
+		musicController = FindObjectOfType<MusicController>();
+		if (switchOnStart) {
+			musicController.SwitchTrack(newTrack);
+			gameObject.SetActive(false);
+		}
+	}
+
+	void Update () {
+
+	}
+
+	void OnTriggerEnter2D(Collider2D other) {
+		if (other.gameObject.name == "Player") {
+			musicController.SwitchTrack(newTrack);
+			gameObject.SetActive(false);
+		}
+	}
+}
