606c1d2fece4183f861f95daf0c819d2aa738ae9
Larlyn Tanasap <strawww@gmail.com>
2017-10-18 13:31:37 -0700

    Add volume controls

---
+++ Assets/Scripts/VolumeController.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeController : MonoBehaviour {
+	private AudioSource audioSource;
+	private float audioLevel;
+	public float defaultAudio;
+
+	void Start () {
+		audioSource = GetComponent<AudioSource>();
+	}
+
+	void Update () {
+
+	}
+
+	public void SetAudioLevel(float volume) {
+		if (audioSource == null) {
+			audioSource = GetComponent<AudioSource>();
+		}
+		audioLevel = defaultAudio * volume;
+		audioSource.volume = audioLevel;
+	}
+}
---
+++ Assets/Scripts/VolumeManager.cs
@@ -0,0 +1,24 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class VolumeManager : MonoBehaviour {
+	public VolumeController[] volumeControllers;
+	public float maxVolume = 1.0f;
+	public float currentVolume;
+
+	void Start () {
+		volumeControllers = FindObjectsOfType<VolumeController>();
+		if (currentVolume > maxVolume) {
+			currentVolume = maxVolume;
+		}
+
+		for (int i = 0; i < volumeControllers.Length; i++) {
+			volumeControllers[i].SetAudioLevel(currentVolume);
+		}
+	}
+
+	void Update () {
+
+	}
+}
