42dff6c9b99d67e5c22177ce6243e8c542033513
Larlyn Tanasap <strawww@gmail.com>
2017-10-18 13:31:53 -0700

    Add gold and coin pickup

---
+++ Assets/Scripts/MoneyManager.cs
@@ -0,0 +1,25 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class MoneyManager : MonoBehaviour {
+	public Text moneyText;
+	public int currentGold;
+
+	void Start () {
+		if (PlayerPrefs.HasKey("CurrentMoney")) {
+			currentGold = PlayerPrefs.GetInt("CurrentMoney");
+		} else {
+			currentGold = 0;
+			PlayerPrefs.SetInt("CurrentMoney", 0);
+		}
+		moneyText.text = "GOLD: " + currentGold;
+	}
+
+	public void UpdateMoney(int moneyChange) {
+		currentGold += moneyChange;
+		PlayerPrefs.SetInt("CurrentMoney", currentGold);
+		moneyText.text = "GOLD: " + currentGold;
+	}
+}
